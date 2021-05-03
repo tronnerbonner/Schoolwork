aedb8e21c5a0f4a504a49dcd500eb2ff125fc1f1
tronnerbonner <59627722+tronnerbonner@users.noreply.github.com>
2021-05-03 12:39:24 -0600

    Added DMIT2015 projects

--- JPA and Integration Testing with Arquillian/dmit2015-1202-oa01-assignment01-njackson11-main/dmit2015-1202-oa01-assignment01-njackson11-main/src/main/java/dmit2015/norrisjackson/config/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package dmit2015.norrisjackson.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
--- JPA and Integration Testing with Arquillian/dmit2015-1202-oa01-assignment01-njackson11-main/dmit2015-1202-oa01-assignment01-njackson11-main/src/main/resources/META-INF/sql/import-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Characters(name,health,fate,versionAdded,lastModifiedDateTime,createdDateTime) VALUES('Ragna',18000,'Blazblue',1.0,'2021-02-04 6:12','2021-02-04 6:12');
-INSERT INTO Characters(name,health,fate,versionAdded,lastModifiedDateTime,createdDateTime) VALUES('Tager',20000,'Blazblue',1.0,'2021-02-04 6:12','2021-02-04 6:12');
-INSERT INTO Characters(name,health,fate,versionAdded,lastModifiedDateTime,createdDateTime) VALUES('Yuzuriha',18000,'UNIST',1.3,'2021-02-04 6:12','2021-02-04 6:12');
